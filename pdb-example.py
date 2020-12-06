ac045a92fed9950cf4e984e1066c28b003d0268c
bsch0111 <bsch0111@naver.com>
2020-12-07 00:12:00 +0900

    breakpoint 연습

---
+++ pdb-example.py
@@ -0,0 +1,29 @@
+
+# import pdb
+
+# a = None
+
+# for i in range(10) : 
+#     if i == 4 :
+#         a = 'Hi'
+#         print('a is set to', a )
+#     elif i == 5:
+#         pdb.set_trace()
+
+# a = []
+
+# for i in range(10) :
+#     a.append(i)
+#     if i == 6 :
+#         breakpoint()
+
+# print(a)
+
+a = None
+
+for i in range(10) : 
+    if i == 4 :
+        a = 'Hi'
+        print('a is set to', a )
+    elif i == 5:
+        breakpoint()
